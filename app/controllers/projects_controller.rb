93a3cbfe83b33d89da9ac06606c7f798c342419a
Brian Chen <380024675@qq.com>
2016-07-03 16:23:01 +0800

    Projects can now be updated

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,6 +5,9 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
   end
+  def edit
+    @project = Project.find(params[:id])
+  end
   def new
     @project = Project.new
   end
@@ -17,6 +20,16 @@ class ProjectsController < ApplicationController
     else
       flash.now[:alert] = "Project has not been created."
       render "new"
+    end
+  end
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+    flash[:notice] = "Project has been updated."
+    redirect_to @project
+    else
+      flash.now[:alert] = "Project has not been updated."
+      render "edit"
     end
   end
 
