4e9eac76e43bb7990c2a34ac8a58e62425da9a79
Brian Chen <380024675@qq.com>
2016-07-29 17:41:44 +0800

    Only admins can create or delete projects

--- spec/features/admin/deleting_projects_spec.rb
+++ spec/features/admin/deleting_projects_spec.rb
@@ -1,5 +1,8 @@
 require "rails_helper"
 RSpec.feature "Users can delete projects" do
+  before do
+    login_as(FactoryGirl.create(:user, :admin))
+  end
   scenario "successfully" do
     FactoryGirl.create(:project, name: "Sublime Text 3")
     visit "/"
@@ -8,4 +11,5 @@ RSpec.feature "Users can delete projects" do
     expect(page).to have_content "Project has been deleted."
     expect(page.current_url).to eq projects_url
     expect(page).to have_no_content "Sublime Text 3"
-  end end
+  end
+end
