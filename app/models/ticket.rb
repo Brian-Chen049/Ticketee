15ed5b19655b920c091dc42c44e72ed03ce4079d
Brian Chen <380024675@qq.com>
2016-08-19 14:42:12 +0800

    Users can now upload 3 files at a time when creating a

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,6 +2,7 @@ class Ticket < ActiveRecord::Base
   belongs_to :project
   belongs_to :author, class_name: "User"
   validates :name, presence: true
+  has_many :attachments, dependent: :destroy
+  accepts_nested_attributes_for :attachments, reject_if: :all_blank
   validates :description, presence: true, length: {minimum: 10}
-  mount_uploader :attachment, AttachmentUploader
 end
