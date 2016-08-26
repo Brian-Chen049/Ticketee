eb29f2051c45f3317811b923159a439acd7fadc1
Brian Chen <380024675@qq.com>
2016-08-26 16:57:44 +0800

    Provide an 'Add another file' link that uses JavaScript   so that users can upload more than one file

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   resources :projects, only: [:index, :show, :edit, :update] do
     resources :tickets
   end
-  resources :attachments, only: [:show]
+  resources :attachments, only: [:show, :new]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
